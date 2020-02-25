274179062f7123f465091d77b2076985630ea4c9
Ryuchen <chenhaom1993@hotmail.com>
2020-02-25 14:12:09 +0800

    update README.md

---
+++ common/plugins/storage/mongostorage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ==================================================
+# @Time : 2020/2/25-13:58
+# @Author : Ryuchen
+# @Site : https://ryuchen.github.io
+# @File : mongostorage
+# @Desc : 
+# ==================================================
